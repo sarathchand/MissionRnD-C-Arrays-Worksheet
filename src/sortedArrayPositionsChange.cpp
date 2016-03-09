d753b988f387751ec71d427e50fd7ffb7f35bc7d
sarathchand <sarathchand@missionrnd.com>
2016-03-09 19:54:37 +0530

    Submit Commit

--- src/sortedArrayPositionsChange.cpp
+++ src/sortedArrayPositionsChange.cpp
@@ -15,5 +15,51 @@ NOTES:
 
 void * sortedArrayPositionsChange(int *Arr, int len)
 {
-	return NULL;
+	int i, pos = -1, p, j = 0, pos1 = -1, t, flag1 = 0, flag2 = 0;
+	if (len>1 && Arr != NULL)
+	{
+		for (i = 0; i < len - 1; i++)
+		{
+			if (i == 0)
+			{
+				if (Arr[i]>Arr[i + 1])
+				{
+					pos = i;
+					flag1 = 1;
+					break;
+				}
+			}
+			else
+			{
+				if (!((Arr[i] < Arr[i + 1]) && (Arr[i] > Arr[i - 1])))
+				{
+					pos = i;
+					flag1 = 1;
+					break;
+				}
+			}
+		}
+		p = Arr[pos];
+		for (i = len - 1; i > pos; i--)
+		{
+			if (!(Arr[i] > Arr[i - 1]))
+			{
+				pos1 = i;
+				flag2 = 1;
+				break;
+			}
+
+		}
+		if (flag1 == 1 && flag2 == 1)
+		{
+			t = Arr[pos];
+			Arr[pos] = Arr[pos1];
+			Arr[pos1] = t;
+		}
+		return Arr;
+	}
+	else
+	{
+		return NULL;
+	}
 }
